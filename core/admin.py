b80c134a269959e273fab37feeb9cf9c305b6929
Alexander Lee <Alexander@Alexanders-MacBook-2.local>
2020-05-11 19:54:14 -0700

    core models: Author, Category, Journal

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Author,
+    Category,
+    Journal,
+)
+
+class JournalAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Journal
+    list_display = [
+        'title',
+        'author',
+        'get_my_categories',
+        'publish_date',
+        'views',
+        'reviewed',
+    ]
+
+
+
+admin.site.register(Author)
+admin.site.register(Category)
+admin.site.register(Journal, JournalAdmin)
+
+
+
+
